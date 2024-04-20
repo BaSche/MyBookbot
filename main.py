0e77a546691a608e5bb4d1378b6dfbb8d5e9081e
BaSche <15978503+BaSche@users.noreply.github.com>
2024-04-20 21:15:55 +0200

    little cleanup

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from collections import Counter
 
 def main():
     path = "books/frankenstein.txt"
+    
     content = read_book(path)
-    words = count_words(content)
-    # print(count_chars(content))
-    filtered = filter_chars_for_alpha(count_chars(content))
-    sorted_chars = dict(sorted(filtered.items(), key=lambda pair: pair[1],reverse=True))
+    words = len(content.split())
+
+    filtered = filter_chars_for_alpha( Counter(content.lower()) )
+    sorted_chars = dict( sorted(filtered.items(), key=lambda pair: pair[1],reverse=True) )
+    
     report(path, words, sorted_chars)
 
 def report(path, words, char_counter):
@@ -21,13 +23,6 @@ def read_book(path):
     with open(path) as book:
         content = book.read()
         return content
-
-def count_words(text):
-    return len(text.split())
-
-def count_chars(text):
-    counter=Counter(text.lower())
-    return counter
 
 def filter_chars_for_alpha (counter):
     return dict(filter(lambda pair: pair[0].isalpha() , counter.items()))
